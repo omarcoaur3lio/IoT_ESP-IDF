108eefe2849e43f21852c9975c4bd56e1b2fac68
omarcoaur3lio <sgtmarcoaurelio@icloud.com>
2022-03-31 17:02:15 -0300

    :construction: Start GenIoT project

--- FreeRTOS/ProjectGenIoT/main.ino
+++ FreeRTOS/ProjectGenIoT/main.ino
@@ -8,7 +8,7 @@
 #include "WiFi.h"
 #include "WiFiMulti.h"
 #include "HTTPClient.h"
-#include "libs/geniotHTTPS_ESP32.h"
+#include "geniotHTTPS_ESP32.h"
 
 void prvSetupHardware(void);
 void vTask1(void *pvParameters);
@@ -16,8 +16,8 @@ void vTask1(void *pvParameters);
 #define CORE_0 0
 #define CORE_1 1
 
-#define WIFISSID "your_SSID"
-#define PASSWORD "wifi_password"
+#define WIFISSID "your_ssid"
+#define PASSWORD "your_pass"
 
 #define TOKEN_GENIOT "your_token"
 #define TEMPERATURA "S00"
@@ -29,7 +29,47 @@ Geniot client((char *)TOKEN_GENIOT);
 
 void prvSetupHardware() {
     Serial.begin(115200);
+loop: 
+    for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(500);
+    }
+    if(!(client.wifiConnection((char *)WIFISSID, (char *)PASSWORD))) {
+        goto loop;
+    }
 }
 
-void setup() {}
-void loop() {}
+void vTask1(void *pvParameters) {
+    float value;
+    char text[100];
+    client.vPrintString("[TASK1] STARTED\n");
+
+    for ( ;; ) {
+        client.add(TEMPERATURA, random(300), (char *)"Test message");
+        client.add(UMIDADE, random(100), (char *)"New message");
+        
+        if(client.send()) {
+            client.vPrintString((char *)"Send success!");
+        }
+
+        if (client.read((char *)TEMPERATURA)) {
+            client.vPrintStringAndFloat((char *) "Value: ", client.geniotValue);
+            client.vPrintTwoStrings((char *) "Message: ", client.geniotText);
+        }
+
+        if (client.read((char *)UMIDADE)) {
+            client.vPrintStringAndFloat((char *) "Value: ", client.geniotValue);
+            client.vPrintTwoStrings((char *) "Message: ", client.geniotText);
+        }
+        vTaskDelay(60000 / portTICK_PERIOD_MS);
+    }
+}
+
+void setup() {
+    prvSetupHardware();
+    xTaskCreatePinnedToCore(vTask1, "Task 1", configMINIMAL_STACK_SIZE + 10000, NULL, 2, NULL, CORE_0);
+}
+void loop() {
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+}
